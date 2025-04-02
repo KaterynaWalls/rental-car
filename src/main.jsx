7daed8ced9354020f1e1ddc7063c7330dc03cf34
Kateryna <katerynawalls@proton.me>
2025-04-01 20:22:21 -0500

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./redux/store";
+import App from "./App.jsx";
+import "./index.css";
+import "modern-normalize";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>
+);
