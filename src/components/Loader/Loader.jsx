0ae29410f26b1c3f9c1e645f9c0c5353afe971a3
Kateryna <katerynawalls@proton.me>
2025-04-02 00:43:13 -0500

    update

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,26 +1,7 @@
-import { RotatingLines } from "react-loader-spinner";
-
-const Loader = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <RotatingLines
-        visible={true}
-        height="96"
-        width="96"
-        color="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
-      />
-    </div>
-  );
-};
+const Loader = () => (
+  <div className="flex justify-center items-center h-screen">
+    <span className="loading loading-spinner text-primary w-16 h-16"></span>
+  </div>
+);
 
 export default Loader;
