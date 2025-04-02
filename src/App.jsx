0ceff274029960b202898334167a1fe6fea40e89
Kateryna <katerynawalls@proton.me>
2025-04-02 00:37:07 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout/Layout.jsx";
+import Layout from "./components/Layout.jsx";
 import { Toaster } from "react-hot-toast";
 import { Suspense, lazy } from "react";
 
