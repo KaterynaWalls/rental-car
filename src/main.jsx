3d5adf4963b74fe9758485640c663380952b880d
Kateryna <katerynawalls@proton.me>
2025-04-01 21:54:55 -0500

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import "modern-normalize";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-      <PersistGate persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </BrowserRouter>
