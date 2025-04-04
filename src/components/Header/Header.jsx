a28627ff819ac39ecad5474d9cc2c3710f577579
Kateryna <katerynawalls@proton.me>
2025-04-03 23:04:07 -0500

    CatalogCard, redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import sprite from "../../assets/images/sprite.svg";
+import s from "./Header.module.css";
 
-function Header() {
-  return <div></div>;
-}
+const getActiveClass = ({ isActive }) =>
+  isActive ? `${s.link} ${s.active}` : s.link;
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <Link to="/">
+        <svg className={s.iconLogo}>
+          <use xlinkHref={`${sprite}#icon-Logologo-1`} />
+        </svg>
+      </Link>
+
+      <nav className={s.navContainer}>
+        <ul className={s.navList}>
+          <li className={s.navItem}>
+            <NavLink to="/" className={getActiveClass}>
+              Home
+            </NavLink>
+          </li>
+          <li className={s.navItem}>
+            <NavLink to="/catalog" className={getActiveClass}>
+              Catalog
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
